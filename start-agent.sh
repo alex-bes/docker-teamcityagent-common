16e161ee3647c195de55c26e115a7a41eb6fb5d9
Dmitry Berezovsky <dmitry.berezovsky@logicify.com>
2015-06-24 14:01:58 +0300

    Improved script for updating property file

--- start-agent.sh
+++ start-agent.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 echo "Updating property file..."
 
-sed -i "s|serverUrl=http://localhost:8111/|serverUrl=$TEAMCITY_SERVER_URL|" /srv/teamcity-agent/conf/buildAgent.properties
-sed -i "s|workDir=../work|workDir=/home/app/work|" /srv/teamcity-agent/conf/buildAgent.properties
-sed -i "s|ownPort=9090|ownPort=$AGENT_PORT|" /srv/teamcity-agent/conf/buildAgent.properties
-sed -i "s|name=|name=$AGENT_NAME|" /srv/teamcity-agent/conf/buildAgent.properties
+sed -i "s|^serverUrl=.*$|serverUrl=$TEAMCITY_SERVER_URL|" /srv/teamcity-agent/conf/buildAgent.properties
+sed -i "s|^workDir=.*$|workDir=/home/app/work|" /srv/teamcity-agent/conf/buildAgent.properties
+sed -i "s|^ownPort=.*$|ownPort=$AGENT_PORT|" /srv/teamcity-agent/conf/buildAgent.properties
+sed -i "s|^name=.*$|name=$AGENT_NAME|" /srv/teamcity-agent/conf/buildAgent.properties
 
 echo "UPDATED:"
 cat /srv/teamcity-agent/conf/buildAgent.properties
